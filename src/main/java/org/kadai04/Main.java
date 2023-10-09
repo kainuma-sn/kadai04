ad4f7a47c0efc206d3cfdec9b2388c12a348f3a1
Shunta Kainuma <arias230461@gmail.com>
2023-10-09 15:26:38 +0900

    Update 04_java by kainuma.sn 　コメントアウトした際に消えてしまった部分を再度インポートしなおしました。 　FunctionCatalog　193行目で呼び出したメソッド名に誤りがあったため、修正しました。

--- src/main/java/org/kadai04/Main.java
+++ src/main/java/org/kadai04/Main.java
@@ -1,6 +1,9 @@
 package org.kadai04;
 
+import Function.FunctionCatalog;
+import Function.FunctionError;
 import Function.FunctionInstruments;
+import Function.FunctionSleeping;
 import Interface.InterfaceMyService;
 
 public class Main {
@@ -13,20 +16,20 @@ public class Main {
         instruments.start();
 
         //Instrumentsを管理する
-//        FunctionCatalog.start();
-//        //外から呼び出しても使えます
-//        FunctionCatalog piano = new FunctionCatalog();
-//        piano.addList("ピアノ", 11, 1);
-//        piano.printInfo(0);
-//
-//        //デバッカーは独自に使いました
-//
-//        ////その他
-//        //マルチスレッド処理
-//        FunctionSleeping.start(); //FunctionCatalogへの実装間に合わず。実行してマルチスレッドの処理を確認することはできます
-//
-//        //例外と例外処理の練習
-//        FunctionError.start();
+        FunctionCatalog.start();
+        //外から呼び出しても使えます
+        FunctionCatalog piano = new FunctionCatalog();
+        piano.addList("ピアノ", 11, 1);
+        piano.printInfo(0);
+
+        //デバッカーは独自に使いました
+
+        ////その他
+        //マルチスレッド処理
+        FunctionSleeping.start(); //FunctionCatalogへの実装間に合わず。実行してマルチスレッドの処理を確認することはできます
+
+        //例外と例外処理の練習
+        FunctionError.start();
 
 
     }

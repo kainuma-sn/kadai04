d894870f459bd7f9e89fa7944141bd987ce5397b
Shunta Kainuma <arias230461@gmail.com>
2023-10-09 15:54:42 +0900

    Update 04_java by kainuma.sn 　ObjectCatalog.javaの121行目をシングルトンになるよう修正 　FunctionCatalog.javaの11行目のコメントを修正 　ObjectCatalog.javaの128行目の不要な記述を削除 　Main.javaの不要な記述を削除

--- src/main/java/org/kadai04/Main.java
+++ src/main/java/org/kadai04/Main.java
@@ -1,33 +1,14 @@
 package org.kadai04;
 
 import Function.FunctionInstruments;
-import Interface.InterfaceMyService;
 
 public class Main {
-    InterfaceMyService myService;
 
     public static void main(String[] args) {
 
         //マルチスレッドでInstrumentsを管理する
         FunctionInstruments instruments = new FunctionInstruments();
         instruments.start();
-//
-//        //Instrumentsを管理する
-//        FunctionCatalog.start();
-//        //外から呼び出しても使えます
-//        FunctionCatalog piano = new FunctionCatalog();
-//        piano.addList("ピアノ", 11, 1);
-//        piano.printInfo(0);
-//
-//        //デバッカーは独自に使いました
-//
-//        ////その他
-//        //マルチスレッド処理
-//        FunctionSleeping.start(); //FunctionCatalogへの実装間に合わず。実行してマルチスレッドの処理を確認することはできます
-//
-//        //例外と例外処理の練習
-//        FunctionError.start();
-
 
     }
 }

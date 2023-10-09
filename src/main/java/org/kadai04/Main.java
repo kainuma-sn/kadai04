e10ee69d363517b3ccc43ff09be516cb4c1443ae
Shunta Kainuma <arias230461@gmail.com>
2023-10-09 15:35:41 +0900

    Update 04_java by kainuma.sn 　ObjectCatalog.javaの121行目の記述がシングルトンになっていないため、修正。

--- src/main/java/org/kadai04/Main.java
+++ src/main/java/org/kadai04/Main.java
@@ -1,9 +1,6 @@
 package org.kadai04;
 
-import Function.FunctionCatalog;
-import Function.FunctionError;
 import Function.FunctionInstruments;
-import Function.FunctionSleeping;
 import Interface.InterfaceMyService;
 
 public class Main {
@@ -14,22 +11,22 @@ public class Main {
         //マルチスレッドでInstrumentsを管理する
         FunctionInstruments instruments = new FunctionInstruments();
         instruments.start();
-
-        //Instrumentsを管理する
-        FunctionCatalog.start();
-        //外から呼び出しても使えます
-        FunctionCatalog piano = new FunctionCatalog();
-        piano.addList("ピアノ", 11, 1);
-        piano.printInfo(0);
-
-        //デバッカーは独自に使いました
-
-        ////その他
-        //マルチスレッド処理
-        FunctionSleeping.start(); //FunctionCatalogへの実装間に合わず。実行してマルチスレッドの処理を確認することはできます
-
-        //例外と例外処理の練習
-        FunctionError.start();
+//
+//        //Instrumentsを管理する
+//        FunctionCatalog.start();
+//        //外から呼び出しても使えます
+//        FunctionCatalog piano = new FunctionCatalog();
+//        piano.addList("ピアノ", 11, 1);
+//        piano.printInfo(0);
+//
+//        //デバッカーは独自に使いました
+//
+//        ////その他
+//        //マルチスレッド処理
+//        FunctionSleeping.start(); //FunctionCatalogへの実装間に合わず。実行してマルチスレッドの処理を確認することはできます
+//
+//        //例外と例外処理の練習
+//        FunctionError.start();
 
 
     }
